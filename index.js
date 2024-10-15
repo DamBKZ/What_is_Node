76c806bf7d535c69ece54b0b27f3152155d6de54
Baczkiewicz Damien <baczkiewicz.damien@hotmail.fr>
2024-10-15 11:49:13 +0200

    Additionnal package figlet

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+import figlet from "figlet"
 import chalk from "chalk";
+
+
+figlet('Hello Wilders!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+}
+console.log(chalk.bgRed(data));
+}
+);
+
 
 console.log(chalk.bgRed("Victor"));
 console.log(chalk.bold.green("Damien"));
